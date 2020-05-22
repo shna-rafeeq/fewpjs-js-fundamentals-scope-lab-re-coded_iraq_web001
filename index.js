94e264fd32ac397b6512146cd77c4502460d92cd
shna rafeeq <shnar.majeed@gmail.com>
2020-05-22 17:56:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,22 @@
 
 function myAnimal() {
   const animal = "dog";
+<<<<<<< HEAD
   return animal;
+=======
+  return animal
+>>>>>>> 996d3c21164bbfbf69bc40c88c5948908893eb2f
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+<<<<<<< HEAD
   const animal = "cat";
+=======
+  let aniaml= "cat";
+>>>>>>> 996d3c21164bbfbf69bc40c88c5948908893eb2f
   return animal;
 }
 
